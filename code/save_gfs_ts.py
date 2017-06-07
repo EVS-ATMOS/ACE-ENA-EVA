75ec917c1637eef0ca1d2ee4f4fa0fb53aa29b71
scollis <scollis.acrf@gmail.com>
2017-06-07 12:27:25 -0500

    ADD: Saving a GFS time series
    Saves a t/td time series for GRW to AWS

---
+++ code/save_gfs_ts.py
@@ -0,0 +1,8 @@
+import os
+import imp
+lib_loc = os.path.join(os.path.expanduser('~'), 'projects/ACE-ENA-EVA/code/ena_tools.py')
+ena_tools = imp.load_source('ena_tools', lib_loc)
+
+if __name__ == '__main__':
+    ena_tools.save_latest_gfs_grw_ttd()
+
